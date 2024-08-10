07e0a8684f519eca399c5bc382ad3b750eacf1e5
Jaeuk Jeong <jaeuk@jeuke.com>
2024-08-11 00:06:27 +0900

    Fix Farm List when farms count is not multiples of 5

--- src/components/FarmList/FarmList.jsx
+++ src/components/FarmList/FarmList.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import FarmItem from "./FarmItem";
 import "./FarmList.css";
 
+const farms = [...Array(10)];
+
 const FarmList = () => {
   return (
     <div className="FarmList">
-      {[...Array(20)].map((_, index) => {
+      {farms.map((_, index) => {
         return (
           <FarmItem title={`${index + 1}번째 농장`} description={"채소농장"} />
         );
+      })}
+      {[...Array(5 - (farms.length % 5))].map((_, index) => {
+        return <div style={{ width: "19%" }}></div>;
       })}
     </div>
   );
