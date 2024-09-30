273768337e2b3a82c7a7a84cc4176b4c34732c23
Jaeuk Jeong <jaeuk@jeuke.com>
2024-10-01 01:14:27 +0900

    Add Logout Button and Link Server

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Row, Col, List, Divider, Card, Avatar } from "antd";
+import React, { useContext, useEffect, useState } from "react";
+import { Row, Col, List, Divider, Card, Avatar, Button } from "antd";
 import {
   UserOutlined,
   IdcardOutlined,
@@ -7,6 +7,8 @@ import {
 } from "@ant-design/icons";
 import { gray } from "@ant-design/colors";
 import ContentHeader from "../components/ContentHeader";
+import { IsLoginContext } from "../contexts/IsLoginContext";
+import { useNavigate } from "react-router-dom";
 
 const point = [
   "물주기 - 1번 구역 물주기. 토양 습도 24%에서 27%로 상승!",
@@ -18,80 +20,128 @@ const point = [
 
 const mydata = ["기본 정보", "주소"];
 
-const MyPage = () => (
-  <>
-    <ContentHeader title={"마이"} sub={"포인트를 모으고\n농작물을 키우자!"} />
-    <Row style={{ marginBottom: "20px", textAlign: "left" }}>
-      <Col span={4}>
-        <Avatar size={64} icon={<UserOutlined />} />
-      </Col>
-      <Col span={4}>
+const MyPage = () => {
+  const [myInfo, setMyInfo] = useState([]);
+
+  const navigate = useNavigate();
+
+  const { setIsLogin } = useContext(IsLoginContext);
+
+  useEffect(() => {
+    const fetchFarms = () => {
+      fetch(`http://${process.env.REACT_APP_API_URL}/users/me`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setMyInfo(data.message);
+        })
+        .catch((err) => console.error("Fetching error:", err));
+    };
+
+    fetchFarms();
+  }, []);
+  return (
+    <>
+      <ContentHeader title={"마이"} sub={"포인트를 모으고\n농작물을 키우자!"} />
+      <div
+        style={{
+          marginBottom: "20px",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <Avatar
+            size={64}
+            icon={<UserOutlined />}
+            style={{ marginRight: "20px" }}
+          />
+          <b>{myInfo.name}</b>
+        </div>
+        <Button
+          onClick={() => {
+            localStorage.removeItem("token");
+            setIsLogin(false);
+            window.location.reload();
+          }}
+        >
+          로그아웃
+        </Button>
+      </div>
+      <Row gutter={10}>
+        <Col span={12}>
+          <Card style={cardStyle}>
+            <b>회원구분</b>
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card style={cardStyle}>
+            <b>{myInfo.userGroup}</b>
+          </Card>
+        </Col>
+      </Row>
+      <Divider
+        orientation="left"
+        style={{ marginBottom: "0px", textAlign: "left", color: gray[7] }}
+      >
+        <h5>
+          <IdcardOutlined style={{ marginRight: "5px" }} />내 정보
+        </h5>
+      </Divider>
+      <List
+        style={{ backgroundColor: "white", border: "none" }}
+        bordered
+        dataSource={mydata}
+        renderItem={(item) => (
+          <List.Item
+            style={{ fontWeight: "bold", color: gray[7], fontSize: "10px" }}
+          >
+            {item}
+          </List.Item>
+        )}
+      />
+      <div style={{ marginTop: "30px" }}></div>
+      <Card style={bigCardStyle}>
+        <b>내 포인트</b>
         <br />
-        <b>김위키</b>
-      </Col>
-      <Col span={16}></Col>
-    </Row>
-    <Row gutter={10}>
-      <Col span={12}>
-        <Card style={cardStyle}>
-          <b>회원구분</b>
-        </Card>
-      </Col>
-      <Col span={12}>
-        <Card style={cardStyle}>
-          <b>일반</b>
-        </Card>
-      </Col>
-    </Row>
-    <Divider
-      orientation="left"
-      style={{ marginBottom: "0px", textAlign: "left", color: gray[7] }}
-    >
-      <h5>
-        <IdcardOutlined style={{ marginRight: "5px" }} />내 정보
-      </h5>
-    </Divider>
-    <List
-      style={{ backgroundColor: "white", border: "none" }}
-      bordered
-      dataSource={mydata}
-      renderItem={(item) => (
-        <List.Item
-          style={{ fontWeight: "bold", color: gray[7], fontSize: "10px" }}
-        >
-          {item}
-        </List.Item>
-      )}
-    />
-    <div style={{ marginTop: "30px" }}></div>
-    <Card style={bigCardStyle}>
-      <b>내 포인트</b>
-      <br />
-      <h2>13000포인트</h2>
-    </Card>
-    <Divider
-      orientation="left"
-      style={{ marginBottom: "0px", textAlign: "left", color: gray[7] }}
-    >
-      <h5>
-        <DollarOutlined style={{ marginRight: "5px" }} />
-        포인트 적립 내역
-      </h5>
-    </Divider>
-    <List
-      style={{ backgroundColor: "white", border: "none" }}
-      bordered
-      dataSource={point}
-      renderItem={(item) => (
-        <List.Item
-          style={{ fontWeight: "bold", color: gray[7], fontSize: "10px" }}
-        >
-          {item}
-        </List.Item>
-      )}
-    />
-  </>
-);
+        <h2>{myInfo.point}포인트</h2>
+      </Card>
+      <Divider
+        orientation="left"
+        style={{ marginBottom: "0px", textAlign: "left", color: gray[7] }}
+      >
+        <h5>
+          <DollarOutlined style={{ marginRight: "5px" }} />
+          포인트 적립 내역
+        </h5>
+      </Divider>
+      <List
+        style={{ backgroundColor: "white", border: "none" }}
+        bordered
+        dataSource={point}
+        renderItem={(item) => (
+          <List.Item
+            style={{ fontWeight: "bold", color: gray[7], fontSize: "10px" }}
+          >
+            {item}
+          </List.Item>
+        )}
+      />
+    </>
+  );
+};
 
 const cardStyle = {
   display: "flex",
